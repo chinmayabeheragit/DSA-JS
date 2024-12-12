b4759e7ab4a07e6e366311d3e611fec8a2c3f51f
Chinmya <105976402+chinmayabeheragit@users.noreply.github.com>
2024-12-13 00:36:29 +0530

    basics array

---
+++ arrayTwoD.js
@@ -0,0 +1,14 @@
+// Define a 2D array
+let arr = [[1, 2], [3, 4]];
+
+// Access elements
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+        console.log(arr[i][j]);
+    }
+}
+// Output:
+// 1
+// 2
+// 3
+// 4
