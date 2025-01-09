b271ab1ade91ed3252c11d7a7be8498972c18e73
Chinmya <105976402+chinmayabeheragit@users.noreply.github.com>
2025-01-09 21:23:49 +0530

    push after practice

---
+++ practice/cart.js
@@ -0,0 +1,20 @@
+let cartQuantity = 0; // camel case
+
+//PascalCase = CartQuantity
+//Kebab-case cart-quantity not supprot in js
+//snake_case cart_quantity not used in js
+
+const variable2 = 3 ;  //can't chnage the value assigned in const
+console.log(variable2);
+
+var variable3 = 3;
+console.log(variable3);
+variable3 = 9;
+console.log(variable3); // can be chnangabel
+
+const message = 'stark';
+console.log(message)
+
+// message = 'paper' // can't be change
+
+console.log(typeof variable2, typeof variable3, typeof message);
---
+++ practice/variable.js
@@ -0,0 +1,35 @@
+
+//ex - 1
+let variable = 3;
+console.log(variable);
+
+//ex-2
+let calculation = 2+2;
+console.log(calculation);
+
+//ex-3
+console.log(calculation +2);
+
+//ex-4
+let result = calculation + 2;
+console.log(result)
+
+//ex-5
+let message = 'hello froom java script';
+console.log(message); console.log(message);
+
+
+//ex-6
+variable = 5;
+console.log(variable);
+
+//ex-7
+variable = variable + 1;
+console.log(variable)
+
+// let create a new variable
+// we can't use let in let like let let = 2; we can use let let1 =3;
+//we can't use like nam2e use can use name2
+// we can't use special charcahter between the variable
+// in java script semi collon means end of the instruction
+// 
