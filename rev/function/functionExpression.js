6635a182a8a94387fa43d15fb5dac377499b1c41
Chinmya <105976402+chinmayabeheragit@users.noreply.github.com>
2025-05-05 23:05:22 +0530

    push

---
+++ rev/function/functionExpression.js
@@ -0,0 +1,6 @@
+var foo = function foo2(){
+    
+    console.log(foo2)
+    console.log("chinmaya");
+}
+foo();
---
+++ rev/function/namedFunction.js
@@ -0,0 +1,40 @@
+// so normally if we define a function like 
+/*
+function (){
+}
+
+ so this is called anonymous function because it doesn't have a name.
+but if we define a function like this
+function foo(){
+    console.log("chinmaya")
+}
+so this is called named function because it has a name.
+but if we define a function like this
+var foo = function(){
+    console.log("chinmaya") 
+
+}
+so this is called function expression because it is assigned to a variable.
+but if we define a function like this
+
+var foo = function foo2(){
+    console.log(foo2)
+    console.log("chinmaya")
+}
+so this is called function statement because it is assigned to a variable.
+
+and also like 
+
+function name (){
+    console.log("chinmaya")
+}
+so this is called dunction statement because it is defined with a name.
+
+but in function expression we can call the function before it is defined because it is hoisted to the top of the scope.
+but in function statement we cannot call the function before it is defined because it is not hoisted to the top of the scope.
+but in named function we can call the function before it is defined because it is hoisted to the top of the scope.
+but in anonymous function we cannot call the function before it is defined because it is not hoisted to the top of the scope.
+
+we can call a anonymous function in function expression but we cannot call a anonymous function in function statement.
+
+ */
