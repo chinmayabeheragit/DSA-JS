df02598e6baca8a706453b8d351d69f83586d341
Chinmaya Behera <chinmayabehera3243@gmail.com>
2026-07-23 13:25:21 +0530

    loop practice

---
+++ loop1.js
@@ -0,0 +1,70 @@
+
+{/* normal 1 to 10 count */}
+// for(let i = 0; i < 11; i++){
+//     console.log(i);
+// }
+
+
+{/* even numbers from 1 to 10 */}
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+{/* odd numbers from 1 to 10 */}
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+// }
+
+{/* increment by 2*/}
+// for(let i = 0; i<10; i+=2){
+//     console.log(i);
+// }
+
+
+{/* decrement from 20 to 0 */}
+// for(let i = 20; i>=0; i--){
+//     console.log(i);
+// }
+
+{/*divisible by 3 */}
+// for(let i = 0; i<30; i++){
+//     if(i % 3 === 0){
+//         console.log(i);
+//     }
+// }
+
+{/*divisible by 5 */}
+// for(let i = 0; i < 30; i++){
+//     if(i% 5 === 0){
+//         console.log(i);
+//     }
+// }
+
+{/*divisible by both 3 and 5 (i.e., by 15) */}
+// for(let i = 0; i < 30; i++){
+//     if(i % 15 === 0){
+//         console.log(i);
+//     }
+// }
+
+{/* print from 1 to N */}
+// let N = 10;
+// for(let i = 1; i <= N; i++){
+//     console.log(i);
+// }
+// for(let i = N; i>=1; i--){
+//     console.log(i);
+// }
+
+{/*find some of 1 to N  */}
+// let N = 20;
+// let sum = 0;
+// for(let i = 1; i <= N; i++){
+//     sum += i;
+
+// }
+// console.log(sum);
