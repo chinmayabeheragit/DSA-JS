016914ddb08bbe215f925ef3901777068f8547f4
Chinmaya Behera <chinmayabehera3243@gmail.com>
2026-07-23 15:12:42 +0530

    loop practice phase 2

--- loop1.js
+++ loop1.js
@@ -1,70 +1,104 @@
 
-{/* normal 1 to 10 count */}
-// for(let i = 0; i < 11; i++){
-//     console.log(i);
-// }
+
+for(let i = 0; i < 11; i++){
+    console.log(i);
+}
 
 
-{/* even numbers from 1 to 10 */}
-// for (let i = 1; i <= 10; i++) {
-//     if (i % 2 === 0) {
-//         console.log(i);
-//     }
-// }
 
-{/* odd numbers from 1 to 10 */}
-// for (let i = 1; i <= 10; i++) {
-//     if (i % 2 !== 0) {
-//         console.log(i);
-//     }
-// }
-
-{/* increment by 2*/}
-// for(let i = 0; i<10; i+=2){
-//     console.log(i);
-// }
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
 
 
-{/* decrement from 20 to 0 */}
-// for(let i = 20; i>=0; i--){
-//     console.log(i);
-// }
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
 
-{/*divisible by 3 */}
-// for(let i = 0; i<30; i++){
-//     if(i % 3 === 0){
-//         console.log(i);
-//     }
-// }
 
-{/*divisible by 5 */}
-// for(let i = 0; i < 30; i++){
-//     if(i% 5 === 0){
-//         console.log(i);
-//     }
-// }
+for(let i = 0; i<10; i+=2){
+    console.log(i);
+}
 
-{/*divisible by both 3 and 5 (i.e., by 15) */}
-// for(let i = 0; i < 30; i++){
-//     if(i % 15 === 0){
-//         console.log(i);
-//     }
-// }
 
-{/* print from 1 to N */}
-// let N = 10;
-// for(let i = 1; i <= N; i++){
-//     console.log(i);
-// }
-// for(let i = N; i>=1; i--){
-//     console.log(i);
-// }
 
-{/*find some of 1 to N  */}
-// let N = 20;
-// let sum = 0;
-// for(let i = 1; i <= N; i++){
-//     sum += i;
+for(let i = 20; i>=0; i--){
+    console.log(i);
+}
 
-// }
-// console.log(sum);
+
+for(let i = 0; i<30; i++){
+    if(i % 3 === 0){
+        console.log(i);
+    }
+}
+
+
+for(let i = 0; i < 30; i++){
+    if(i% 5 === 0){
+        console.log(i);
+    }
+}
+
+
+for(let i = 0; i < 30; i++){
+    if(i % 15 === 0){
+        console.log(i);
+    }
+}
+
+
+let N = 10;
+for(let i = 1; i <= N; i++){
+    console.log(i);
+}
+for(let i = N; i>=1; i--){
+    console.log(i);
+}
+
+
+let N = 20;
+let sum = 0;
+for(let i = 1; i <= N; i++){
+    sum += i;
+
+}
+console.log(sum);
+
+
+ let N = 6;
+let product = 1;
+for(let i = 1; i<=N; i++){
+    product = product * i;
+}
+console.log(product);
+
+
+
+let n = 19;
+for(let i = 1; i<= 10; i++){
+    console.log(n + " x " + i + " = " + (n * i));
+}
+
+
+let N = 1000;
+let count = 0;
+for(let i = 1; i <= N; i++)
+{
+    count++;
+}
+console.log(count);
+
+
+for(let i = 1; i<=10; i++){
+    console.log(i*i);
+}
+
+
+for(let i = 1; i<=10; i++){
+    console.log(i*i*i);
+}
